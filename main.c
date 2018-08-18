9871f247e4b1d0424b6810af2b961aa52cd3861c
gadepall <gadepall@gmail.com>
2018-08-18 14:32:52 +0530

    Add existing files

--- main.c
+++ main.c
@@ -1,95 +1,57 @@
-/*
-  Copyright (c) 2015, Augusto Fraga Giachero
-  All rights reserved.
+//Blink LED
+//GVV Sharma
+//July 30, 2018
 
-  Redistribution and use in source and binary forms, with or without
-  modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of the <organization> nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
+#include "stm32f10x_lib.h"
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_rcc.h"
 
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  DISCLAIMED. IN NO EVENT SHALL AUGUSTO FRAGA GIACHERO BE LIABLE FOR ANY
-  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
-/*
- * Blink a LED
- */
-#include "stm32f103xb.h"
-
-/*
- * Delay function:
- * Uses the SysTick timer to wait for
- * an arbitrary time in microseconds.
- *
- * The clock source is assumed to be
- * the internal 8MHz RC oscillator
- * divided by 8 (1MHz)
- */
-void delay_us(unsigned int time)
+//Setup GPIO pins
+void Init_GPIO(void)
 {
-	/*
-	 * Load the delay period in microseconds
-	 * assuming a 1MHz source
-	 */
-	SysTick->LOAD = time;
+  GPIO_InitTypeDef  GPIO_InitStructure;
 
-	/*
-	 * Clears the current value and the count flag
-	 */
-	SysTick->VAL = 0;
-	
-	/*
-	 * Waits until the count ends
-	 */
-	while(!(SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk));
-}
+  /* Initialize LED which connected to PC13 */
+  // Enable PORTC Clock
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+  /* Configure the GPIO_LED pin */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOC, &GPIO_InitStructure);
 
-int main()
+  //GPIO_SetBits(GPIOC, GPIO_Pin_13); // Set C13 to High level ("1")
+  GPIO_ResetBits(GPIOC, GPIO_Pin_13); // Set C13 to Low level ("0")
+
+}//end GPIO pin setup
+
+void delay_time(uint32_t n_time)
 {
-	/*
-	 * Enable all Ports and Alternate Function clocks
-	 */
-	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPBEN |
-	    RCC_APB2ENR_IOPCEN | RCC_APB2ENR_IOPDEN | RCC_APB2ENR_AFIOEN;
-	
-	/*
-	 * Disable JTAG and SWO (Free PB3, PB4 and PA15)
-	 */
-	AFIO->MAPR = AFIO_MAPR_SWJ_CFG_JTAGDISABLE;
-
-	/*
-	 * Enable the SysTick Timer with
-	 * the CPU clock divided by 8
-	 */
-	SysTick->CTRL = SysTick_CTRL_ENABLE_Msk;
-
-	/*
-	 * Enable the PA1 as a digital output
-	 */
-	GPIOA->CRL = 0x00000020;
-
-	/*
-	 * Infinite loop
-	 */
-	while(1)
-	{
-		GPIOA->BSRR = GPIO_BSRR_BS1; //PA1 = 1 (Led OFF)
-		delay_us(500000); //500ms delay
-		GPIOA->BSRR = GPIO_BSRR_BR1; //PA1 = 0 (Led ON)
-		delay_us(500000); //500ms delay
-	}
+	int i;
+	for(i=0;i<n_time;i++);
 }
+int main(void)
+{
+  
+
+Init_GPIO(); //configure gpio pins
+
+//begin blink
+  while (1) {
+
+    	/* Toggle LED  connected to PC13*/
+    	GPIOC->ODR ^= GPIO_Pin_13; // Invert C13
+
+    	/* delay */
+		delay_time(0x100000);
+	
+
+    	/* Toggle LED  connected to PC13*/
+    	GPIOC->ODR ^= GPIO_Pin_13;
+
+    	/* delay */
+    	delay_time(0x100000);
+
+  }//end blink
+  
+}//end main
