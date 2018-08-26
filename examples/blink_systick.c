71e4bf31c77745371cd64a03c3e9b0192d2110ac
gadepall <gadepall@gmail.com>
2018-08-26 11:31:05 +0530

    Add existing files

--- examples/blink_systick.c
+++ examples/blink_systick.c
@@ -1,52 +1,17 @@
-/*
-  Copyright (c) 2015, Augusto Fraga Giachero
-  All rights reserved.
-
-  Redistribution and use in source and binary forms, with or without
-  modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of the <organization> nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
-
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  DISCLAIMED. IN NO EVENT SHALL AUGUSTO FRAGA GIACHERO BE LIABLE FOR ANY
-  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
-/*
- * Blink a LED
+/* GVV Sharma 
+ * August 24, 2018
+ * 
+ * Blink a LED using HSI clock
+ * and SysTick timer
  */
 #include "stm32f103xb.h"
 
-/*
- * Delay function:
- * Uses the SysTick timer to wait for
- * an arbitrary time in microseconds.
- *
- * The clock source is assumed to be
- * the internal 8MHz RC oscillator
- * divided by 8 (1MHz)
- */
-void delay_us(unsigned int time)
+void delay_us(uint32_t time)
 {
 	/*
 	 * Load the delay period in microseconds
-	 * assuming a 1MHz source
 	 */
 	SysTick->LOAD = time;
-
 	/*
 	 * Clears the current value and the count flag
 	 */
@@ -55,7 +20,8 @@ void delay_us(unsigned int time)
 	/*
 	 * Waits until the count ends
 	 */
-	while(!(SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk));
+//	while(!(SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk));
+	while(!(SysTick->CTRL & 0x00010000));
 }
 
 int main()
@@ -75,7 +41,8 @@ int main()
 	 * Enable the SysTick Timer with
 	 * the CPU clock divided by 8
 	 */
-	SysTick->CTRL = SysTick_CTRL_ENABLE_Msk;
+//	SysTick->CTRL = 0x00000001; //1MHz clock
+	SysTick->CTRL = 0x00000005;	//8MHz clock
 
 	/*
 	 * Enable the PA1 as a digital output
@@ -88,8 +55,8 @@ int main()
 	while(1)
 	{
 		GPIOA->BSRR = GPIO_BSRR_BS1; //PA1 = 1 (Led OFF)
-		delay_us(500000); //500ms delay
+		delay_us(4000000); //500ms delay
 		GPIOA->BSRR = GPIO_BSRR_BR1; //PA1 = 0 (Led ON)
-		delay_us(500000); //500ms delay
+		delay_us(4000000); //500ms delay
 	}
 }
--- main.c
+++ main.c
@@ -1,93 +1,31 @@
-/*
-GVV Sharma, August 21, 2018
-Code released under GNU GPL
-*/
-
-/*
- * Binary to Decimal
+/* GVV Sharma 
+ * August 24, 2018
+ * 
+ * Blink a LED using HSI clock
+ * and SysTick timer
  */
-#include "stm32f103xb.h"	
+#include "stm32f103xb.h"
 
-uint8_t get_bit(uint16_t pin)
+void delay_us(uint32_t time)
 {
-		if (((GPIOB->IDR & pin) == 0 ))  
-			return 0;
-		else
-			return 1;
+	/*
+	 * Load the delay period in microseconds
+	 */
+	SysTick->LOAD = time;
+	/*
+	 * Clears the current value and the count flag
+	 */
+	SysTick->VAL = 0;
 	
-}
-
-uint8_t bin2dec(uint8_t Z, uint8_t Y,uint8_t X,uint8_t W )
-{
-	return W+2*X+4*Y+8*Z;
-}
-
-void sevenseg(uint8_t dec)
-{
-	switch(dec)
-	{
-	case 0:
-			GPIOB->BRR = (1<<4)|(1<<5)|(1<<6)|(1<<7)|(1<<8)|(1<<9); // (Led ON)		
-			GPIOB->BSRR = (1<<3); // (Led OFF)					
-			break;
-
-	case 1:
-			GPIOB->BRR = (1<<7)|(1<<8); // (Led ON)		
-			GPIOB->BSRR = (1<<3)|(1<<4)|(1<<5)|(1<<6)|(1<<9); // (Led OFF)					
-			break;
-
-	case 2:
-			GPIOB->BRR = (1<<3)|(1<<5)|(1<<6)|(1<<8)|(1<<9); // (Led ON)		
-			GPIOB->BSRR = (1<<4)|(1<<7); // (Led OFF)					
-			break;
-
-	case 3:
-			GPIOB->BRR = (1<<3)|(1<<6)|(1<<7)|(1<<8)|(1<<9); // (Led ON)		
-			GPIOB->BSRR = (1<<4)|(1<<5); // (Led OFF)					
-			break;
-
-	case 4:
-			GPIOB->BRR = (1<<3)|(1<<4)|(1<<7)|(1<<8); // (Led ON)		
-			GPIOB->BSRR = (1<<5)|(1<<6)|(1<<9); // (Led OFF)					
-			break;
-
-	case 5:
-			GPIOB->BRR = (1<<3)|(1<<4)|(1<<6)|(1<<7)|(1<<9); // (Led ON)		
-			GPIOB->BSRR = (1<<5)|(1<<8); // (Led OFF)					
-			break;
-
-	case 6:
-			GPIOB->BRR = (1<<3)|(1<<4)|(1<<5)|(1<<6)|(1<<7)|(1<<9); // (Led ON)		
-			GPIOB->BSRR = (1<<8); // (Led OFF)					
-			break;
-
-	case 7:
-			GPIOB->BRR = (1<<7)|(1<<8)|(1<<9); // (Led ON)		
-			GPIOB->BSRR = (1<<3)|(1<<4)|(1<<5)|(1<<6); // (Led OFF)					
-			break;
-
-	case 8:
-			GPIOB->BRR = (1<<3)|(1<<4)|(1<<5)|(1<<6)|(1<<7)|(1<<8)|(1<<9); // (Led ON)		
-			break;
-
-	case 9:
-			GPIOB->BRR = (1<<3)|(1<<4)|(1<<6)|(1<<7)|(1<<8)|(1<<9); // (Led ON)		
-			GPIOB->BSRR = (1<<5); // (Led OFF)					
-			break;
-	default:
-			GPIOB->BRR = (1<<3)|(1<<4)|(1<<5)|(1<<6)|(1<<9); // (Led ON)		
-			GPIOB->BSRR = (1<<7)|(1<<8); // (Led OFF)					
-	
-			break;
-
-
-	}
+	/*
+	 * Waits until the count ends
+	 */
+//	while(!(SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk));
+	while(!(SysTick->CTRL & 0x00010000));
 }
 
 int main()
 {
-	uint8_t W,X,Y,Z;
-
 	/*
 	 * Enable all Ports and Alternate Function clocks
 	 */
@@ -100,22 +38,25 @@ int main()
 	AFIO->MAPR = AFIO_MAPR_SWJ_CFG_JTAGDISABLE;
 
 	/*
-	 * Enable the PB3-PB9 as a digital output and
-	 * Enable the PB12-PB15 as a digital output
+	 * Enable the SysTick Timer with
+	 * the CPU clock divided by 8
 	 */
-	GPIOB->CRL = 0x33333000;
-	GPIOB->CRH = 0x88880033;		
+//	SysTick->CTRL = 0x00000001; //1MHz clock
+	SysTick->CTRL = 0x00000005;	//8MHz clock
+
+	/*
+	 * Enable the PA1 as a digital output
+	 */
+	GPIOA->CRL = 0x00000020;
 
 	/*
 	 * Infinite loop
 	 */
 	while(1)
 	{
-		W = get_bit(0x1000);//B12
-		X = get_bit(0x2000);//B13
-		Y = get_bit(0x4000);//B14
-		Z = get_bit(0x8000);//B15
-		sevenseg(bin2dec(Z,Y,X,W));
-
+		GPIOA->BSRR = GPIO_BSRR_BS1; //PA1 = 1 (Led OFF)
+		delay_us(4000000); //500ms delay
+		GPIOA->BSRR = GPIO_BSRR_BR1; //PA1 = 0 (Led ON)
+		delay_us(4000000); //500ms delay
 	}
 }
